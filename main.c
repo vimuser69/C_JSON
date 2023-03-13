a30d1f738cbba293bd466895e23c915bdd728718
vimuser69 <126293545+vimuser69@users.noreply.github.com>
2023-03-13 16:16:56 -0700

    Update main.c

--- main.c
+++ main.c
@@ -35,14 +35,24 @@ char *clean_json(char *json) {
 }
 
 char *get_keys(char *json) {
-    bool in_key = false;
     size_t next_key = 0;
     size_t keys = 0;
-    char array[10][10];
     for (size_t i=0; i<strlen(json); i++) {
         if (json[i] == '{' || json[i] == ',' && json[i+1] == '\"') {
             next_key = i+2;
             keys++;
+        }
+    }
+    char **result = malloc(sizeof(char *) * keys);
+    bool in_key = false;
+    size_t on_key = -1;
+    size_t key_size = 0;
+    next_key = 0;
+    for (size_t i=0; i<strlen(json); i++) {
+        if (json[i] == '{' || json[i] == ',' && json[i+1] == '\"') {
+            next_key = i+2;
+            on_key++;
+            on_key -= on_key/2;
         }
         if (json[i] == '\"' && json[i+1] == ':') {
             in_key = false;
@@ -51,14 +61,45 @@ char *get_keys(char *json) {
             in_key = true;
         }
         if (in_key == true) {
-            array[keys]
+            key_size++;
+        }
+        if (in_key == false && key_size > 0) {
+            result[on_key] = malloc(key_size);
+            on_key++;
+            key_size = 0;
         }
     }
+    in_key = false;
+    on_key = -1;
+    key_size = 0;
+    next_key = 0;
+    for (size_t i=0; i<strlen(json); i++) {
+        if (json[i] == '{' || json[i] == ',' && json[i+1] == '\"') {
+            next_key = i+2;
+            on_key++;
+            on_key -= on_key/2;
+        }
+        if (json[i] == '\"' && json[i+1] == ':') {
+            in_key = false;
+        }
+        if (i == next_key) {
+            in_key = true;
+        }
+        if (in_key == true) {
+            result[on_key][key_size] = json[i];
+            key_size++;
+        }
+        if (in_key == false && key_size > 0) {
+            on_key++;
+            key_size = 0;
+        }
+    }
+    printf("%s\n", result[0]);
 }
 
 int main() {
-    char *data = "{\"test1\" : \"bababooey\", \"test2\" : \"memes2\", \"test3\" : 69420}";
-    //printf("%s\n", clean_json(data));
+    char *data = "{\"test69\" : \"bababooey\", \"test2\" : \"memes2\", \"test3\" : 69420}";
+    printf("%s\n", clean_json(data));
     get_keys(clean_json(data));
     return 0;
 }
